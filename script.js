066259e0b6e9ffa726454db859ccd25cefb04f61
Vu-PH29926 <134067248+Vu-PH29926@users.noreply.github.com>
2023-10-13 11:20:40 +0700

    lab5-6

--- script.js
+++ script.js
@@ -1,32 +1,77 @@
-const gameEvents =  new Map([
-    [17, 'Goal'],
-    [36, 'Substitution'],
-    [47, 'Goal'],
-    [61, 'Substitutiom'],
-    [64, 'Yellow Card'],
-    [69, 'Red card'],
-    [70, 'Substistution'],
-    [72, 'Substitution'],
-    [76, 'Goal'],
-    [80, 'Goal'],
-    [92, 'Yellow Cảd'],  
+// const  btnScrollTo =  document.querySelector('.btn-scroll-to');
+// const section1 = document.querySelector('#section-1');
+// btnScrollTo.addEventListener('click', function (e){
+//     const s1corrds = section1.getBoundingClientRect();
+//     console.log(s1corrds);
 
-]);
-//1
-const events = new Set(gameEvents.values());
-console.log(events);
+//     console.log(e.target.getBoundingClientRect());
 
-//2
-gameEvents.delete(64);
+//     console.log('current sroll (X/Y' ,window.pageXOffset, pageYOffset);
 
-//3
-console.log(`An event happened , on average , event ${90 / gameEvents.size} minutes`);
-const time  = [...gameEvents.keys()].pop();
-console.log(time);
-console.log(`An event happened , on average , event ${time / gameEvents.size} minutes`);
+//     console.log(
+//         'height/width viewport',
+//         document.documentElement.clientHeight,
+//         document.documentElement.clientWidth,
+//     );
 
-//4
-for (const [min,event] of gameEvents){
-    const half  = min <= 45 ? 'first' : 'second';
-    console.log(`[${half} half] ${min} : ${event}`);
-}
+//     section1.scrollIntoView({behavior: 'smooth'});
+// });
+
+//5.3
+// const radomInt = (min,max) =>
+//   Math.floor(Math.random() * (max - min  + 1) + min );
+// const radomColor = () =>
+// `rgb(${radomInt(0,255)},${radomInt(0,255)},${radomInt()})`;
+
+
+// document.querySelector('.nav-link').addEventListener
+// ('click', function (e){
+//     this.style.backgroundColor = radomColor();
+//     console.log('LINK',e.target);
+// });
+
+// document.querySelector('.nav-link').addEventListener
+// ('click', function (e){
+//     this.style.backgroundColor = radomColor();
+//     console.log('CONTAINER',e.target);
+// });
+
+// document.querySelector('.nav-link').addEventListener
+// ('click', function (e){
+//     this.style.backgroundColor = radomColor();
+//     console.log('NAV',e.target);
+// });
+
+//5.4
+section1.scrollIntaView({behavior:'smooth'});
+
+//page navigaion
+// document.querySelectorAll('.nav__link').forEach
+// (function(e){
+//     el.addEventListener('click', function(e){
+//         e.preventDefault();
+//         const id = this.getAttribute('href');
+//         console.log(id);
+//         document.querySelector(id).scrollIntaView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+//1.Add event listener to common parent element 
+//2.Determine what element originated the event 
+
+document.querySelector('.nav__links').addEventListener
+('click',function(e){
+    e.preventDefault();
+
+    //matching strateg
+    if(e.target.classList.contains('nav__libk')){
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntaView({
+            behavior : 'smooth'
+        })
+    }
+});
+
+ 
